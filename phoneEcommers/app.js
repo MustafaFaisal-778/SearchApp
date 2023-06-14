980c08a08f18c5d2b07c9677c8f00ffecbf4ed4f
Mustafa Faisal <122450274+MustafaFaisal-778@users.noreply.github.com>
2023-06-14 23:55:52 +0500

    Add files via upload

---
+++ phoneEcommers/app.js
@@ -0,0 +1,338 @@
+var phones = {
+    iphone:{
+        iphone12Pro:{
+            name: "iphone 12 PRO",
+            price:"300,000",
+            quality: "NEW",
+            img:"img/apple/apple 1.webp",
+        },
+        iphone11:{
+            name: "iphone 11 ",
+            price:"100,000",
+            quality: "NEW",
+            img:"img/apple/apple 11 2.webp",
+        },
+        iphone13:{
+            name: "iphone 13 PRO",
+            price:"250,000",
+            quality: "NEW",
+            img:"img/apple/13 3.webp",
+        },
+        iphone14:{
+            name: "iphone 14",
+            price:"345,000",
+            quality: "NEW",
+            img:"img/apple/14 4.webp",
+        },
+        iphone14Pro:{
+            name: "iphone 14 PRO",
+            price:"300,000",
+            quality: "NEW",
+            img:"img/apple/14 pro 5.webp",
+        },
+        iphone6s:{
+            name: "iphone 6s",
+            price:"30,000",
+            quality: "USED",
+            img:"img/apple/6s 6.webp",
+        },
+        iphoneX:{
+            name: "iphone X",
+            price:"250,000",
+            quality: "NEW",
+            img:"img/apple/X 7.webp",
+        },
+        iphone5s:{
+            name: "iphone 5s ",
+            price:"200,000",
+            quality: "NEW",
+            img:"img/apple/5s 8.webp",
+        },
+    },
+    Samsung:{
+        SamsungA14:{
+            name:"Samsung Galaxy A14",
+            price:"150,000",
+            quality:"NEW",
+            img:"img/Samsung/a14 1.webp",
+        
+        },
+
+        SamsungM04:{
+            name:"Samsung Galaxy M04",
+            price:"150,000",
+            quality:"NEW",
+            img:"img/Samsung/samsung-galaxy-m04- 5.webp",
+        
+        },
+        SamsungS20:{
+            name:"Samsung Galaxy S20",
+            price:"150,000",
+            quality:"NEW",
+            img:"img/Samsung/samsung-galaxy-s20-7.webp",
+        
+        },
+        SamsungS21:{
+            name:"Samsung Galaxy S21",
+            price:"150,000",
+            quality:"NEW",
+            img:"img/Samsung/samsung-galaxy-s21-6.webp",
+        
+        },
+        Samsungs22:{
+            name:"Samsung Galaxy S22",
+            price:"150,000",
+            quality:"NEW",
+            img:"img/Samsung/samsung-galaxy-s22-ultra- 4.webp",
+        
+        },
+        SamsungS23:{
+            name:"Samsung Galaxy S23",
+            price:"150,000",
+            quality:"NEW",
+            img:"img/Samsung/samsung-galaxy-s23 3.webp",
+        
+        },
+        
+    },
+    oppo:{
+       oppoA7:{
+        name:"Oppo A7",
+        price:"245,000",
+        quality:"NEW",
+        img:"img/oppo/oppo-a7-6.webp",
+
+       } ,
+       oppoA16:{
+        name:"Oppo A16",
+        price:"245,000",
+        quality:"NEW",
+        img:"img/oppo/oppo-a16-1.webp",
+
+       } ,
+       oppoA31:{
+        name:"Oppo A31",
+        price:"245,000",
+        quality:"NEW",
+        img:"img/oppo/oppo-a31-7.webp",
+
+       } ,
+       oppoA57s:{
+        name:"Oppo A57 ",
+        price:"245,000",
+        quality:"NEW",
+        img:"img/oppo/oppo-a57s-3.webp",
+
+       } ,
+       oppoA97:{
+        name:"Oppo A97",
+        price:"245,000",
+        quality:"NEW",
+        img:"img/oppo/oppo-a97-4.webp",
+
+       } ,
+       oppoF17:{
+        name:"Oppo F17",
+        price:"245,000",
+        quality:"NEW",
+        img:"img/oppo/oppo-f17-5.webp",
+
+       } ,
+       oppof21:{
+        name:"Oppo F21",
+        price:"245,000",
+        quality:"NEW",
+        img:"img/oppo/oppo-f21-2.webp",
+
+       } ,
+       oppoReno8t:{
+        name:"Oppo Reno8 ",
+        price:"245,000",
+        quality:"NEW",
+        img:"img/oppo/oppo-reno8-t-5g-8.webp",
+
+       } ,
+
+
+    },
+    infinix:{
+        infinixHot12:{
+            name:"Infinix Hot 12",
+            price:"90,000",
+            quality:"New",
+            img:"img/infinix/infinix-hot-12-2.webp",
+
+        },
+        infinixHot12I:{
+            name:"Infinix Hot 12i",
+            price:"90,000",
+            quality:"New",
+            img:"img/infinix/infinix-hot-12i-1.webp",
+
+        },
+        infinixHot20s:{
+            name:"Infinix Hot 20s",
+            price:"90,000",
+            quality:"New",
+            img:"img/infinix/infinix-hot-20s-5.webp",
+
+        },
+        infinixNote12:{
+            name:"Infinix Note 12",
+            price:"90,000",
+            quality:"New",
+            img:"img/infinix/infinix-note-12-3.webp",
+
+        },
+        infinixS5Pro:{
+            name:"Infinix s5 Pro",
+            price:"90,000",
+            quality:"New",
+            img:"img/infinix/infinix-s5-pro-7.webp",
+
+        },
+        infinixSmart6:{
+            name:"Infinix Smart 6",
+            price:"90,000",
+            quality:"New",
+            img:"img/infinix/infinix-smart-6-4.webp",
+
+        },
+        infinixSmart7:{
+            name:"Infinix Smart 7",
+            price:"90,000",
+            quality:"New",
+            img:"img/infinix/infinix-smart-7-8.png",
+
+        },
+        infinixZeroXNeo:{
+            name:"Infinix Zero X Neo",
+            price:"90,000",
+            quality:"New",
+            img:"img/infinix/infinix-zero-x-neo-6.webp",
+
+        },
+        
+    },
+    itel:{
+        itelA23Pro:{
+            name:"Itel A23 Pro",
+            price:"20,000",
+            quality:"New",
+            img:"img/itel/itel-a23-pro-4.webp",
+        },
+        itelA27:{
+            name:"Itel A27",
+            price:"20,000",
+            quality:"New",
+            img:"img/itel/itel-a27-1.webp",
+        },
+        itelMuzik400:{
+            name:"Itel Muzik 400",
+            price:"2,000",
+            quality:"Used",
+            img:"img/itel/itel-muzik-400-3.webp",
+        },
+
+        itelp17Pro:{
+            name:"Itel P17 Pro",
+            price:"20,000",
+            quality:"New",
+            img:"img/itel/itel-p17-pro-5.webp",
+        },
+        itelvalue100:{
+            name:"Itel Value 100s",
+            price:"20,000",
+            quality:"Used",
+            img:"img/itel/itel-value-100s-6.webp",
+        },
+        itelValue110:{
+            name:"Itel Value 110",
+            price:"20,000",
+            quality:"Used",
+            img:"img/itel/itel-value-110-2.webp",
+        },
+      
+
+    },
+
+
+
+
+
+}
+
+
+
+
+card=document.getElementById("card")
+// categories=document.getElementById("categories")
+function allCards(){
+    for(var main in phones){
+        for(key in phones[main]){
+            // for(values in phones[main][key]){
+                // console.log(key)
+                // console.log(phones[main][key].name)
+
+                card.innerHTML+=`
+                <div class="card-body" id="card">
+                <div class="img-section">
+                <img src="${phones[main][key].img}" alt="">
+                </div>
+                
+                <hr />
+                <div class="text-section">
+                  <h6>${phones[main][key].name}</h6>
+                  <span>RS ${phones[main][key].price}</span>
+                  <p>${phones[main][key].quality}</p>
+                </div>
+    
+    
+              </div>
+                `
+            // }
+        }
+    }
+    
+    
+}
+allCards()
+
+card=document.getElementById("card")
+function select(phone){
+    card.innerHTML=""
+    for(main in phones[phone]){
+        console.log(main)
+        for(key in phones[phone][main])
+        console.log(phones[phone][main].name)
+
+        card.innerHTML+=`
+                <div class="card-body" id="card">
+                <div class="img-section">
+                <img src="${phones[phone][main].img}" alt="">
+                </div>
+                
+                <hr />
+                <div class="text-section">
+                  <h6>${phones[phone][main].name}</h6>
+                  <span>RS ${phones[phone][main].price}</span>
+                  <p>${phones[phone][main].quality}</p>
+                </div>
+    
+    
+              </div>
+                `
+
+    }
+
+}
+
+search=document.getElementById('search')
+// search.preventDefault()
+function submit(searxh){
+    searxh.preventDefault()
+    console.log(search.value)
+}
+
+submit(searxh)
+
